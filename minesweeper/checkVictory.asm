2b1e2b287bc0c47661a8e23c0c5ad94a4b90504a
everton <146574629+ev3rton0@users.noreply.github.com>
2024-02-08 10:48:26 -0300

    Update checkVictory.asm

--- minesweeper/checkVictory.asm
+++ minesweeper/checkVictory.asm
@@ -6,29 +6,48 @@ checkVictory:
 	save_context
 	move $s0, $a0
 	
-	li $v0, 0 #cont = 0
+	li $v0, 0 # count = 0
 	
 	li $s3, 0 # i = 0
 	li $s4, SIZE 
 	
 	fordoi:
-		bgt $s3, $s4, finaldofordoi
+		bge $s3, $s4, finalfordoi
 		
-	li $s5, 0
-	li $s6, SIZE
+		li $s5, 0
+		li $s6, SIZE
+		
 	fordoj:
-		bgt $s5, $s6, finaldofordoj
+		bge $s5, $s6,finalfordoj
 		
-	#if aq
-	sll $t1, $s3, 5
-	sll $t2, $s5, 2
+		#if aq
+		sll $t1, $s3, 5 # t1 = i*32
+		sll $t2, $s5, 2 #
 	
-	addi $t0, $t1, $t2
-	addi $t0, $t0, $s0
-	lw $s7, 0($t0)
+		add $t0, $t1, $t2
+		add $t0, $t0, $s0
+		
+		lw $s7, 0($t0)
 	
-	blt $s7, $zero, continua 
+		bge $s7, 0, continua 
 	
-	addi $v0, $v0, 1
+		addi $v0, $v0, 1    # Incrementa count
+			
+		continua:
+			addi $s5, $s5, 1    # Incrementa j
+			j fordoj
 	
-	 
+	finalfordoj:
+		addi $s3, $s3, 1        # Incrementa i
+		j fordoi
+	
+	finalfordoi:
+	
+		beq $v0,BOMB_COUNT, retorne1
+		li $v0,0
+		jr $ra                 
+  
+	retorne1:
+		li $v0, 1
+		restore_context
+		jr $ra
